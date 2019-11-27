f91664536b6972498256778d5eecbea07e3cf3a3
John McKee <jmckee3@mac.com>
2019-11-26 16:52:23 -0800

    working version

--- sherlock-and-anagrams.go
+++ sherlock-and-anagrams.go
@@ -1,3 +1,44 @@
 package main
 
+import (
+	"sort"
+)
+
+func sherlockAndAnagrams(s string) int32 {
+	sLength := int32(len(s))
+	var count int32
+	var sortedBaseString string
+	var sortedComparedToString string
+
+	for w := int32(0); w < sLength-1; w++ {
+		for i := int32(0); i < sLength-w-1; i++ {
+			sortedBaseString = SortString(s[i : i+w+1])
+			for j := i + 1; j < sLength-w; j++ {
+				sortedComparedToString = SortString(s[j : j+w+1])
+				if sortedBaseString == sortedComparedToString {
+					count++
+				}
+			}
+		}
+	}
+
+	return count
+}
+
+func StringToRuneSlice(s string) []rune {
+	var r []rune
+	for _, runeValue := range s {
+		r = append(r, runeValue)
+	}
+	return r
+}
+
+func SortString(s string) string {
+	r := StringToRuneSlice(s)
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
+	return string(r)
+}
+
 func main() {}
--- spec-sherlock-and-anagrams_test.go
+++ spec-sherlock-and-anagrams_test.go
@@ -2,6 +2,39 @@ package main
 
 import "testing"
 
-func TestSherlockAndAnagrams(t *testing.T) {
+func TestSherlockAndAnagrams1(t *testing.T) {
+	s := "abba"
+	// s := "abcdefgh"
+	anagrams := sherlockAndAnagrams(s)
 
+	if anagrams != 4 {
+		t.Errorf("got %d instead of 4", anagrams)
+	}
 }
+
+func TestSherlockAndAnagrams2(t *testing.T) {
+	s := "abcd"
+	anagrams := sherlockAndAnagrams(s)
+
+	if anagrams != 0 {
+		t.Errorf("got %d instead of 0", anagrams)
+	}
+}
+
+// func TestSherlockAndAnagrams3(t *testing.T) {
+// 	s := "ifailuhkqq"
+// 	anagrams := sherlockAndAnagrams(s)
+
+// 	if anagrams != 3 {
+// 		t.Errorf("got %d instead of 3", anagrams)
+// 	}
+// }
+
+// func TestSherlockAndAnagrams4(t *testing.T) {
+// 	s := "kkkk"
+// 	anagrams := sherlockAndAnagrams(s)
+
+// 	if anagrams != 10 {
+// 		t.Errorf("got %d instead of 10", anagrams)
+// 	}
+// }
